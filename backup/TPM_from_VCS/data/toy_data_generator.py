fixed vcs calculation

backup/TPM_from_VCS/data/toy_data_generator.py
@@ -12,6 +12,8 @@ from TPM_from_VCS.util import calculate_virtual_coordinates as calc_VCS
 def create_dataset(size):
     # dataset variable must be initialised outside the for-loop. Otherwise, only 1 training-datapoint is prepared!
     dataset = []
+    
+    # for debug only:
  
     for i in range(0, size):
         temp = np.random.randint(low=3, high=100)
@@ -21,7 +23,7 @@ def create_dataset(size):
 
         for i in range(0, phy_coordinates.shape[0]):
             dataset.append(((vcs[i]), (phy_coordinates[i])))
-            # print('Size of dataset list : {}'.format(len(dataset)))
+
 
         if i % 50 == 0:
             print('Created {} records in the dataset\n'.format(i))

backup/TPM_from_VCS/util/calculate_virtual_coordinates.py
@@ -2,47 +2,27 @@
 import numpy as np
 
 def generate_physical_coordinates(n):
-    phy_coord = np.random.rand(n, 2)
+    phy_coord = np.random.randn(n, 2)
     return phy_coord
 
 # function ge_VC will be called by external modules to calculate VC
 def distance(x, y, dist_matrix):
-    return np.sqrt((dist_matrix[x][0] - dist_matrix[y][0])**2 + (dist_matrix[x][1] - dist_matrix[y][1])**2) 
+    return np.sqrt((dist_matrix[x, 0] - dist_matrix[y, 0])**2 + (dist_matrix[x, 1] - dist_matrix[y, 1])**2) 
     
 def is_neighbour(x, y, dist_matrix):
     return distance(x, y, dist_matrix) <= 1
 
 
 # finding shortest paths based on Dynamic Programming
-def get_shortest_path(source, dest, dist_matrix, VC_matrix, anchors):
-    # print('DEBUG: Source = {}'.format(source))
-    # print('DEBUG: Destination = {}'.format(dest))
-
-    if VC_matrix[source][dest] != 100:
-        return VC_matrix[source][dest]
-
-    if source == dest:
-        VC_matrix[source][dest] = 0
-        VC_matrix[dest][source] = VC_matrix[source][dest]
-
-        return VC_matrix[source][dest]
-
-
-    # if these nodes are neighbours, then hop_dist =  1
-    if(distance(source, anchors[dest], dist_matrix) <= 1):
-        VC_matrix[source][dest] = 1
-        VC_matrix[dest][source] = VC_matrix[source][dest]
-
-        return VC_matrix[source][dest]
+def get_shortest_path(source, dest, dist_matrix, VC_matrix):
 
     # return 1 + min distance from source to a neighbour of destination
     for _ in range(0, dist_matrix.shape[0]):
         for i in range(0, dist_matrix.shape[0]):
-            if is_neighbour(anchors[dest], i, dist_matrix):
-                VC_matrix[source][dest] = min(VC_matrix[source][dest], 1 + VC_matrix[source][i])
+            if is_neighbour(source, i, dist_matrix):
+                VC_matrix[source][dest] = min(VC_matrix[source][dest], 1 + get_shortest_path(i, dest, dist_matrix, VC_matrix))
 
-    # symmetric distances
-    VC_matrix[dest][source] = VC_matrix[source][dest]
+
     return VC_matrix[source][dest]
 
 # dist_matrix is the matrix of geographical coordinates: Number_of_nodes X 2
@@ -51,23 +31,68 @@ def get_shortest_path(source, dest, dist_matrix, VC_matrix, anchors):
 def calculate_shortest_hops(dist_matrix, anchors):
     VC_matrix = 1000 * np.ones((dist_matrix.shape[0], len(anchors)))
 
-    # print('Length of anchor node array: {}'.format(len(anchors)))
+    for temp in range(len(anchors)):
+        VC_matrix[anchors[temp]][temp] = 0
 
-    # print('DEBUG: dist_matrix {}'.format(dist_matrix.shape))
-    # print('DEBUG: VC_matrix {}'.format(VC_matrix.shape))
+    for source in range(dist_matrix.shape[0]):
+        for dest in range(len(anchors)):
 
-    for i in range(0, dist_matrix.shape[0]):
-        for j in range(0, len(anchors)):
-            VC_matrix[i][j] = get_shortest_path(i, j, dist_matrix, VC_matrix, anchors)
+            if VC_matrix[source][dest] > 0:
+                # if these nodes are neighbours, then hop_dist =  1
+                if(distance(source, anchors[dest], dist_matrix) <= 1):
+                    VC_matrix[source][dest] = 1
+                    # VC_matrix[dest][source] = VC_matrix[source][dest]
 
+    # print('Prelim investigation done: {}'.format(VC_matrix))
+
+    while not processed_all_nodes(VC_matrix, dist_matrix, anchors) and baked_rice(VC_matrix, dist_matrix, anchors):
+        for source in range(0, dist_matrix.shape[0]):
+            for dest in range(0, len(anchors)):
+                for i in range(dist_matrix.shape[0]):
+                    if is_neighbour(source, i, dist_matrix):
+                        VC_matrix[source][dest] = min(VC_matrix[source][dest], 1 + VC_matrix[i][dest])
+
+    print('Completed VC processing')
     return VC_matrix
 
+def baked_rice(VC_matrix, dist_matrix, anchors):
+    for source in range(0, dist_matrix.shape[0]):
+            for dest in range(0, len(anchors)):
+                if VC_matrix[source][dest] == 1000:
+                    for i in range(dist_matrix.shape[0]):
+                        if is_neighbour(source, i, dist_matrix):
+
+                            if VC_matrix[source][dest] > 1 + VC_matrix[i][dest]:
+                                VC_matrix[source][dest] = 1 + VC_matrix[i][dest]
+                                return 0
+
+    return 1
+
+def processed_all_nodes(VC_matrix, dist_matrix, anchors):
+    ''' Function to verify if processing is complete '''
+
+    for source in range(0, dist_matrix.shape[0]):
+            for dest in range(0, len(anchors)):
+                for i in range(dist_matrix.shape[0]):
+                    if is_neighbour(source, i, dist_matrix):
+
+                        if VC_matrix[source][dest] > 1 + VC_matrix[i][dest]:
+                            VC_matrix[source][dest] = 1 + VC_matrix[i][dest]
+                            return 0
+
+    return 1
+
+
+
 def select_anchor_nodes(dist_matrix):
-    return np.random.randint(low=0, high=dist_matrix.shape[0], size=(5, 1, 1))
-    
+    anchor_list = np.random.choice(range(20), 5, replace=False)
+
+    print('Anchor List: {}'.format(anchor_list))
+    return anchor_list
 
 def get_VC(num_of_nodes):
     # print('get_VC: Need to create {} nodes'.format(num_of_nodes))
     dist_matrix = generate_physical_coordinates(num_of_nodes)
+    print('dist_matrix shape : {}'.format(dist_matrix))
     anchors = select_anchor_nodes(dist_matrix)
     return dist_matrix, calculate_shortest_hops(dist_matrix, anchors)